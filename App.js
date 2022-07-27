e5f4eb342d43aea430863b3d283769e9d624d7cd
Tanatat Wongpanyanurak <boomnoobb@gmail.com>
2022-07-27 09:31:52 +0700

    7/27/2022 commit

--- App.js
+++ App.js
@@ -10,10 +10,11 @@ import AlertComponent from './components/AlertComponent'
 import ImageWithTextInput from './components/ImageWithTextInput'
 import ButtonExample from './components/ButtonExample'
 import Touchable_Example from './components/Touchable_Example'
+import TouchablePractice from './components/TouchablePractice'
 
 const App = () => {
   return (
-    <View style ={styles.container}>
+    <View >
       {/* <Logo/> */}
       {/* <User/> */}
       {/* <LotsOfGreetings/> */}
@@ -25,7 +26,9 @@ const App = () => {
         {/* <AlertComponent/> */}
         {/* <ImageWithTextInput/> */}
         {/* <ButtonExample/> */}
-        <Touchable_Example/>
+       {/* <Touchable_Example/> */}
+       <TouchablePractice/>
+      
    </View>
   )
 }
---
+++ components/TouchablePractice.js
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, View,SafeAreaView,
+    TouchableOpacity,Image } from 'react-native'
+import React from 'react'
+
+const TouchablePractice = () => {
+  return (
+    <SafeAreaView style ={styles.container}>
+        <View style ={styles.container}>
+    <View >
+    <TouchableOpacity
+                style = {styles.buttonFacebookStyle}  >
+                    <Image
+            source={require("../assets/facebook.png")}
+            style={styles.buttonImageIconStyle}
+          />
+                         <View style ={styles.buttonIconSeparatorStyle}></View>
+                     <Text style = {styles.buttonTextStyle}>Login Using Facebook</Text>
+                </TouchableOpacity>
+                
+    </View>
+    <View >
+    <TouchableOpacity
+                style = {styles.buttonGPlusStyle}  >
+                    <Image
+            source={require("../assets/google-plus.png")}
+            style={styles.buttonImageIconStyle}
+          />
+                    <View style ={styles.buttonIconSeparatorStyle}></View>
+                     <Text style = {styles.buttonTextStyle}>Login Using Facebook</Text>
+                </TouchableOpacity>
+                
+    </View>
+    </View>
+    </SafeAreaView>
+  )
+}
+
+export default TouchablePractice
+
+const styles = StyleSheet.create({
+    container: {flex: 1,
+        margin: 10,
+        marginTop: 30,
+        padding: 30,
+    },
+    buttonGPlusStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#dc4e41',
+        borderWidth: 0.5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        margin: 5,
+    },
+    buttonFacebookStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#43CCE5',
+        borderWidth: 0.5,
+        borderColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        margin: 5,
+    },
+    buttonImageIconStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+    },
+    buttonTextStyle: {
+        color: '#fff',
+        marginBottom: 4,
+        marginLeft: 10,
+    },
+    buttonIconSeparatorStyle: {
+        backgroundColor: '#fff',
+        width: 1,
+        height: 40,
+    },
+})
